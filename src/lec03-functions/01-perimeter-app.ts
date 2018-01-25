44ec00e027ab7bdbd639dae0594594eeeb9dc532
Kris Jordan <krisjordan@gmail.com>
2018-01-25 11:23:08 -0500

    add perimeter example

--- src/lec03-functions/01-perimeter-app.ts
+++ src/lec03-functions/01-perimeter-app.ts
@@ -1,7 +1,7 @@
 import { print, promptNumber } from "introcs";
 
-// TODO #1: Implement perimeter's return statement expression.
 let perimeter = (length: number, width: number): number => {
+    // TODO #2: Implement perimeter's return statement expression.3
     return 0;
 };
 
@@ -11,7 +11,7 @@ export let main = async () => {
     let length: number = await promptNumber("Length");
     let width: number = await promptNumber("Width");
     
-    // TODO #2: Call the function perimeter
+    // TODO #1: Call the function perimeter
     let result: number = 0;
     
     print(result);
