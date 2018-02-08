f59334953ef1676177e45d15653497d5a16f7c3b
Kris Jordan <krisjordan@gmail.com>
2018-02-08 13:28:36 -0500

    Add lec07 examples

---
+++ src/lec07-recursion-with-numbers/01-sub-app.ts
@@ -0,0 +1,15 @@
+import { print } from "introcs";
+
+export let main = async () => {
+    print(sub(3, 0));
+};
+
+// Challenge: Implement the sub function recursively
+// using only:
+//   + 1 and 
+//   - 1
+let sub = (n: number, m: number): number => {
+    return 0;
+};
+
+main();
