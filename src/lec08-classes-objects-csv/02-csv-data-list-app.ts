8de49b517a78be055f3fc150fc676d5f1e1e2901
Kris Jordan <krisjordan@gmail.com>
2018-02-13 13:29:40 -0500

    Add final example to lec08

--- src/lec08-classes-objects-csv/02-csv-data-list-app.ts
+++ src/lec08-classes-objects-csv/02-csv-data-list-app.ts
@@ -1,6 +1,12 @@
 import { print, csvToList } from "introcs";
 import { List, cons, listify, first, rest } from "introcs/list";
 
+/*
+The Game class declares the properties every Game object
+will have as well as their "default" values. One way to think
+of a property is as the title of a column in a data table. Each
+object, then, has its own row in the table.
+*/
 class Game {
     date: string = "";
     opponent: string = "";
@@ -13,34 +19,43 @@ class Game {
 
 export let main = async () => {
 
+    // The following line prompts the user to load a CSV file.
     let games: List<Game> = await csvToList("Basketball Data", Game);
+    print("Games");
     print(games);
 
+    // Extract just a list of Joel's points per game
+    let points: List<number> = gamesToPoints(games);
+    print("Points");
+    print(points);
+
+    // Total all the points
+    let total: number = addUp(points);
+    print("Total");
+    print(total);
+
 };
 
+/**
+ * The gamesToPoints function, given a list of Games, will
+ * return a list of only the points property of each game.
+ */
 let gamesToPoints = (games: List<Game>): List<number> => {
-    if (games === null) {
-        return null;
-    } else {
-        let game: Game = first(games);
-        return cons(game.points, gamesToPoints(rest(games)));
-    }
+
+    // TODO: Fix this implementation
+    return null;
+
 };
 
-let gamesToUNCPoints = (games: List<Game>): List<number> => {
-    if (games === null) {
-        return null;
-    } else {
-        let game: Game = first(games);
-        return cons(game.uncPoints, gamesToUNCPoints(rest(games)));
-    }
-};
-
-let total = (values: List<number>): number => {
+/**
+ * The addUp function, given a list of numbers, will
+ * return the addUp of all numbers added together.
+ */
+let addUp = (values: List<number>): number => {
     if (values === null) {
         return 0;
     } else {
-        return first(values) + total(rest(values));
+        return first(values) + addUp(rest(values));
     }
 };
 
