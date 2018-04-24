e4cb20f8e7c6321c248f6abedfa926fc2e89e7a0
Kris Jordan <krisjordan@gmail.com>
2018-04-24 09:34:25 -0400

    lec25 draft

---
+++ src/lec25-lists/cons-first-rest-app.ts
@@ -0,0 +1,53 @@
+import { print } from "introcs";
+
+type List<T> = Node<T> | null;
+
+class Node<T> {
+    value: T;
+    next: Node<T> | null;
+
+    constructor(value: T, next: List<T>) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+export let cons = <T> (value: T, next: List<T>): Node<T> => {
+    return new Node(value, next);
+};
+
+export let first = <T> (list: List<T>): T => {
+    if (list !== null) {
+        return list.value;
+    } else {
+        throw new Error("Can't call first on empty list!");
+    }
+};
+
+export let rest = <T> (list: List<T>): List<T> => {
+    if (list !== null) {
+        return list.next;
+    } else {
+        throw new Error("Can't call rest on null!");
+    }
+};
+
+export let listify = <T> (...args: T[]): List<T> => {
+    let list: List<T> = null;
+    for (let i = args.length - 1; i >= 0; i--) {
+        list = cons(args[i], list);
+    }
+    return list;
+};
+
+export let printList = <T> (list: List<T>): void => {
+    if (list === null) {
+        return;
+    } else {
+        print(first(list));
+        printList(rest(list));
+    }
+};
+
+let l = cons(1, cons(2, cons(3, null)));
+printList(listify(1, 2, 3, 4, 5));
